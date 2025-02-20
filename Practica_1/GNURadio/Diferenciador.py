a8b0fb203f6864c1c14958462ca688fc6d3e26e4
jarajara25 <fteefir@gmail.com>
2025-02-20 09:49:04 -0500

    final results

--- Practica_1/GNURadio/Diferenciador.py
+++ Practica_1/GNURadio/Diferenciador.py
@@ -11,7 +11,7 @@
 from PyQt5 import Qt
 from gnuradio import qtgui
 from PyQt5 import QtCore
-from gnuradio import analog
+from gnuradio import blocks
 from gnuradio import gr
 from gnuradio.filter import firdes
 from gnuradio.fft import window
@@ -74,7 +74,7 @@ class Diferenciador(gr.top_block, Qt.QWidget):
             1024, #size
             samp_rate, #samp_rate
             "", #name
-            1, #number of inputs
+            2, #number of inputs
             None # parent
         )
         self.qtgui_time_sink_x_0.set_update_time(0.10)
@@ -105,7 +105,7 @@ class Diferenciador(gr.top_block, Qt.QWidget):
             -1, -1, -1, -1, -1]
 
 
-        for i in range(1):
+        for i in range(2):
             if len(labels[i]) == 0:
                 self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
@@ -119,7 +119,7 @@ class Diferenciador(gr.top_block, Qt.QWidget):
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.epy_block_0 = epy_block_0.blk()
-        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 1000, 1, 0, 0)
+        self.blocks_vector_source_x_1 = blocks.vector_source_f((1,1,1,1,1,1,1,2,2,2,2,2,2,2,2), True, 1, [])
         self._Amplitud_range = qtgui.Range(1, 60000, 1, 1, 200)
         self._Amplitud_win = qtgui.RangeWidget(self._Amplitud_range, self.set_Amplitud, "'Amplitud'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._Amplitud_win)
@@ -128,7 +128,8 @@ class Diferenciador(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.epy_block_0, 0))
+        self.connect((self.blocks_vector_source_x_1, 0), (self.epy_block_0, 0))
+        self.connect((self.blocks_vector_source_x_1, 0), (self.qtgui_time_sink_x_0, 1))
         self.connect((self.epy_block_0, 0), (self.qtgui_time_sink_x_0, 0))
 
 
@@ -145,7 +146,6 @@ class Diferenciador(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
     def get_Amplitud(self):
