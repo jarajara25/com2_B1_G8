b155988cc8cda4572383401197dc43aca3e5b914
jarajara25 <fteefir@gmail.com>
2025-04-24 09:58:55 -0500

    Codigos utilizados para este informe

--- Practica_4/GNURadio/Prueba.py
+++ Practica_4/GNURadio/Prueba.py
@@ -13,7 +13,6 @@ from gnuradio import qtgui
 from gnuradio import analog
 from gnuradio import blocks
 import numpy
-from gnuradio import digital
 from gnuradio import filter
 from gnuradio.filter import firdes
 from gnuradio import gr
@@ -24,13 +23,14 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+import Prueba_epy_block_0 as epy_block_0  # embedded python block
 import math
 import sip
 import threading
 
 
 
-class QPSK(gr.top_block, Qt.QWidget):
+class Prueba(gr.top_block, Qt.QWidget):
 
     def __init__(self):
         gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
@@ -53,7 +53,7 @@ class QPSK(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "QPSK")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "Prueba")
 
         try:
             geometry = self.settings.value("geometry")
@@ -66,43 +66,43 @@ class QPSK(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.Truth_Table_Constelation = Truth_Table_Constelation = (0.34+0.94j,-0.94+0.34j,-0.34-0.94j,0.94-0.34j)
-        self.M = M = len(Truth_Table_Constelation)
-        self.sps = sps = 8
-        self.bps = bps = int(math.log(M,2))
-        self.Rs = Rs = 32000
-        self.samp_rate = samp_rate = 32000
-        self.h = h = [1]*sps
-        self.Rb = Rb = Rs*bps
+        self.M = M = 8
+        self.h = h = (1.,1.,1.,1.,1.,1.,1.,1.,1.,1.,1.)
+        self.Rb = Rb = 32000
+        self.Nbps = Nbps = int(math.log(M,2))
+        self.Sps = Sps = len(h)
+        self.Rs = Rs = Rb/Nbps
+        self.samp_rate = samp_rate = Rs*Sps
+        self.Sps_0 = Sps_0 = len(h)
 
         ##################################################
         # Blocks
         ##################################################
 
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
-            1024, #size
+        self.qtgui_time_sink_x_1_0 = qtgui.time_sink_c(
+            (int(32*Sps/Nbps)), #size
             samp_rate, #samp_rate
             "", #name
             1, #number of inputs
             None # parent
         )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        self.qtgui_time_sink_x_1_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_1_0.set_y_axis(-1, 1)
 
-        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+        self.qtgui_time_sink_x_1_0.set_y_label('Amplitude', "")
 
-        self.qtgui_time_sink_x_0.enable_tags(True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(False)
-        self.qtgui_time_sink_x_0.enable_grid(False)
-        self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
-        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+        self.qtgui_time_sink_x_1_0.enable_tags(True)
+        self.qtgui_time_sink_x_1_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_1_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_1_0.enable_grid(False)
+        self.qtgui_time_sink_x_1_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_1_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_1_0.enable_stem_plot(False)
 
 
-        labels = ['RE', 'IM', 'Signal 3', 'Signal 4', 'Signal 5',
+        labels = ['EC-Re', 'EC-Im', 'Signal 3', 'Signal 4', 'Signal 5',
             'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
-        widths = [1, 1, 1, 1, 1,
+        widths = [3, 3, 1, 1, 1,
             1, 1, 1, 1, 1]
         colors = ['blue', 'red', 'green', 'black', 'cyan',
             'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
@@ -117,19 +117,19 @@ class QPSK(gr.top_block, Qt.QWidget):
         for i in range(2):
             if len(labels[i]) == 0:
                 if (i % 2 == 0):
-                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
+                    self.qtgui_time_sink_x_1_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
                 else:
-                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
+                    self.qtgui_time_sink_x_1_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
             else:
-                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+                self.qtgui_time_sink_x_1_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_1_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_1_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_1_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_1_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_1_0.set_line_alpha(i, alphas[i])
 
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self._qtgui_time_sink_x_1_0_win = sip.wrapinstance(self.qtgui_time_sink_x_1_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_1_0_win)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
             1024, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
@@ -213,87 +213,57 @@ class QPSK(gr.top_block, Qt.QWidget):
 
         self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
-        self.interp_fir_filter_xxx_0 = filter.interp_fir_filter_ccc(sps, h)
+        self.interp_fir_filter_xxx_0 = filter.interp_fir_filter_ccc(Sps, h)
         self.interp_fir_filter_xxx_0.declare_sample_delay(0)
-        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bc(Truth_Table_Constelation, 1)
-        self.blocks_packed_to_unpacked_xx_0 = blocks.packed_to_unpacked_bb(bps, gr.GR_MSB_FIRST)
-        self.blocks_pack_k_bits_bb_0 = blocks.pack_k_bits_bb(8)
+        self.epy_block_0 = epy_block_0.blk()
+        self.blocks_repack_bits_bb_0 = blocks.repack_bits_bb(1, Nbps, "", False, gr.GR_LSB_FIRST)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff((2*math.pi/M))
+        self.blocks_char_to_float_0_1 = blocks.char_to_float(1, 1)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
         self.analog_random_source_x_0 = blocks.vector_source_b(list(map(int, numpy.random.randint(0, 2, 1000))), True)
-        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, (100e-3), 0)
+        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, (50e-3), 0)
+        self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 1.)
 
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.analog_const_source_x_0, 0), (self.epy_block_0, 1))
         self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
-        self.connect((self.analog_random_source_x_0, 0), (self.blocks_pack_k_bits_bb_0, 0))
+        self.connect((self.analog_random_source_x_0, 0), (self.blocks_repack_bits_bb_0, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.qtgui_const_sink_x_0, 0))
-        self.connect((self.blocks_add_xx_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.blocks_pack_k_bits_bb_0, 0), (self.blocks_packed_to_unpacked_xx_0, 0))
-        self.connect((self.blocks_packed_to_unpacked_xx_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))
-        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.interp_fir_filter_xxx_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.qtgui_time_sink_x_1_0, 0))
+        self.connect((self.blocks_char_to_float_0_1, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.epy_block_0, 0))
+        self.connect((self.blocks_repack_bits_bb_0, 0), (self.blocks_char_to_float_0_1, 0))
+        self.connect((self.epy_block_0, 0), (self.interp_fir_filter_xxx_0, 0))
         self.connect((self.interp_fir_filter_xxx_0, 0), (self.blocks_add_xx_0, 0))
-        self.connect((self.interp_fir_filter_xxx_0, 0), (self.qtgui_freq_sink_x_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "QPSK")
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "Prueba")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
 
         event.accept()
 
-    def get_Truth_Table_Constelation(self):
-        return self.Truth_Table_Constelation
-
-    def set_Truth_Table_Constelation(self, Truth_Table_Constelation):
-        self.Truth_Table_Constelation = Truth_Table_Constelation
-        self.set_M(len(self.Truth_Table_Constelation))
-        self.digital_chunks_to_symbols_xx_0.set_symbol_table(self.Truth_Table_Constelation)
-
     def get_M(self):
         return self.M
 
     def set_M(self, M):
         self.M = M
-        self.set_bps(int(math.log(self.M,2)))
-
-    def get_sps(self):
-        return self.sps
-
-    def set_sps(self, sps):
-        self.sps = sps
-        self.set_h([1]*self.sps)
-
-    def get_bps(self):
-        return self.bps
-
-    def set_bps(self, bps):
-        self.bps = bps
-        self.set_Rb(self.Rs*self.bps)
-
-    def get_Rs(self):
-        return self.Rs
-
-    def set_Rs(self, Rs):
-        self.Rs = Rs
-        self.set_Rb(self.Rs*self.bps)
-
-    def get_samp_rate(self):
-        return self.samp_rate
-
-    def set_samp_rate(self, samp_rate):
-        self.samp_rate = samp_rate
-        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.set_Nbps(int(math.log(self.M,2)))
+        self.blocks_multiply_const_vxx_0.set_k((2*math.pi/self.M))
 
     def get_h(self):
         return self.h
 
     def set_h(self, h):
         self.h = h
+        self.set_Sps(len(self.h))
+        self.set_Sps_0(len(self.h))
         self.interp_fir_filter_xxx_0.set_taps(self.h)
 
     def get_Rb(self):
@@ -301,11 +271,48 @@ class QPSK(gr.top_block, Qt.QWidget):
 
     def set_Rb(self, Rb):
         self.Rb = Rb
+        self.set_Rs(self.Rb/self.Nbps)
+
+    def get_Nbps(self):
+        return self.Nbps
+
+    def set_Nbps(self, Nbps):
+        self.Nbps = Nbps
+        self.set_Rs(self.Rb/self.Nbps)
+        self.blocks_repack_bits_bb_0.set_k_and_l(1,self.Nbps)
+
+    def get_Sps(self):
+        return self.Sps
+
+    def set_Sps(self, Sps):
+        self.Sps = Sps
+        self.set_samp_rate(self.Rs*self.Sps)
+
+    def get_Rs(self):
+        return self.Rs
+
+    def set_Rs(self, Rs):
+        self.Rs = Rs
+        self.set_samp_rate(self.Rs*self.Sps)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.qtgui_time_sink_x_1_0.set_samp_rate(self.samp_rate)
+
+    def get_Sps_0(self):
+        return self.Sps_0
+
+    def set_Sps_0(self, Sps_0):
+        self.Sps_0 = Sps_0
 
 
 
 
-def main(top_block_cls=QPSK, options=None):
+def main(top_block_cls=Prueba, options=None):
 
     qapp = Qt.QApplication(sys.argv)
 
